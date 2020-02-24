de27ad9e8673f4870f23dd3f62ccb1b9c58d3af0
ACER <ACER@KhoaNguyen>
2020-02-24 22:07:39 +0700

    Bai67_Static

---
+++ HocOOP_Static/src/communityuni/com/model/XXX.java
@@ -0,0 +1,25 @@
+package communityuni.com.model;
+
+public class XXX {
+	private static int a;
+	public void setA(int value)
+	{
+		a = value;
+	}
+	
+	public int getA()
+	{
+		return a;
+	}
+	
+	public static void fn1()
+	{
+		
+	}
+	
+	public void fn2()
+	{
+		
+	}
+
+}
